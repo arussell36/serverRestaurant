abdec627b24286e8ef6ef08e2d0002f6c30e534c
arussell36 <andrewrussell036@gmail.com>
2019-11-23 13:45:53 -0600

    final rest

--- server.js
+++ server.js
@@ -2,82 +2,76 @@
 // ===========================================================
 const express = require("express");
 const app = express();
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const path = require("path");
 
-
-app.use(express.urlencoded({extend: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
 // Data
 // ===========================================================
 
-const characters = [{
-  routeName: "yoda",
-  name: "Yoda",
-  role: "Jedi Master",
-  age: 900,
-  forcePoints: 2000
-},
-{
-  routeName: "darthmaul",
-  name: "Darth Maul",
-  role: "Sith Lord",
-  age: 200,
-  forcePoints: 1200
+const reservation = [{
+    custName: "bo-bob",
+    custEmail: "bobob@gmail.com",
+    custID: "412312312",
+    custPhone: "321.123.2312",
+},{
+    custName: "mo-bamba",
+    custEmail: "moooo@gmail.com",
+    custID: "51341412",
+    custPhone: "321.231.2452"
 }];
 
-// Create one more data entry for the character Obi Wan Kenobi.
-// Enter any values you like for the parameters following the same format as the Yoda and Darth Maul character
-//
+const waitList = [{
+    custName: "wait bo-bob",
+    custEmail: "bobob@gmail.com",
+    custID: "412312312",
+    custPhone: "321.123.2312",
+}];
 
-// YOUR CODE GOES HERE
-
-//
 
 // Routes
 // ===========================================================
+// Site Grab
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "view.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/add", (req, res) => {
-  res.sendFile(path.join(__dirname, "add.html"));
+app.get("/reservations", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/reservations.html"));
 });
 
-app.get("/api/characters/:character", (req, res) => {
-  const chosen = req.params.character;
+// ===========================================================
+// API Grab
+app.get("/api/reservation", (req, res) => {
+    res.json(reservation);
+    // res.json("YOU MADE IT");
+});
 
-  const chosenOne = characters.filter(obj => {
-    return obj.routeName === chosen
+app.get("/api/waitlist", (req, res) => {
+    res.json(waitList);
   });
 
-  if (chosenOne.length) {
-    return res.json(chosenOne[0]);
-  } else {
-  return res.send('wat, who you lookin for?');
-  }
-});
-
-app.get("/api/characters", (req, res) => {
-  return res.json(characters);
-  
-});
-
-
-app.post("/api/characters", (req, res) => {
-  const newCharacter = req.body;
-  
-  newCharacter.routeName - newCharacter.name.replace(/\s+/g, "").toLowerCase();
-
-  characters.push(newCharacter);
-  console.log(newCharacter);
-  res.json(newCharacter);
-
-})
-
+// ===========================================================
+// API Grab
+app.post("/api/tables", function(req, res) {
+// Getting the raw data from the client //
+// We need to format the data to fit in the database /
+    if (reservation.length < 5) {
+        reservation.push(req.body);
+        res.json(reservation)
+        console.log("hi");
+    } else {
+        waitList.push(req.body);
+        res.json(waitList);
+// You need the below json because it sends back a string to let the server or user know the info was sent/received //
+        console.log("hello");
+}});
 
 // Listener
 // ===========================================================
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("Reservations listening on PORT " + PORT);
 });
